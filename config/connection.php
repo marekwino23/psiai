1fd628fcb26f3e21252053a1085f3169b9a298a4
Marek <marekw1996@gmail.com>
2019-11-23 14:12:13 +0100

    register, login and display name when logged in or register message when you just got registered

--- config/connection.php
+++ config/connection.php
@@ -5,6 +5,7 @@
     try {
         $db = new PDO('mysql:host=localhost;dbname=newDB', USERNAME, PASSWORD);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        session_start();
     } catch (PDOException $e) {
         $db->query('CREATE DATABASE newDB');
         print "Error!: " . $e->getMessage() . "<br/>";
--- index.php
+++ index.php
@@ -1,18 +1,38 @@
-<?php require_once('./config/connection.php'); ?>
+<?php require_once('./config/connection.php'); 
+    if(isset($_POST['logout'])) {
+        session_destroy();
+        header('Location: index.php');
+        $db = null;
+    }
+?>
 <html>
 <head>
-<title> Strona główna </title>
+<title>Strona główna</title>
 <meta charset = "UTF-8">
 <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <?php require_once('./layout/header.php'); ?>
     <?php 
+
+    if(!empty($_SESSION['email'])) {
+        echo 'Witaj '.$_SESSION['email'].'';
+        echo '<form method="POST">
+        <button name="logout">logout</button>
+        </form>';
+    }
+
+    if(empty($_SESSION['email'])) {
+        if(isset($_SESSION['register'])) {
+        echo "Dziekujemy za rejestracje. Możesz teraz sie zalogowac";
+        session_destroy();
+        }
             echo '<h2>Logowanie</h2>    
         <div class="main">
             <a class="btn" href="./user/login.php">ZALOGUJ</a>
             <a class="btn" href="./user/register.php">ZAREJESTRUJ</a>
         </div>';
+    }
     require_once('./layout/footer.php') ?>
 </body>
 </html>
